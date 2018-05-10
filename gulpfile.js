54a1faf63e67e135a36163ce069cba9eaf4cf813
Igor <yaindex2@yandex.ru>
2018-05-10 18:31:51 +0300

    Completed Automatic Sprite generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,3 @@
 require ('./gulp/tasks/styles');
 require ('./gulp/tasks/watch');
-/*
-gulp1.task('default', function(){
-	console.log("Hooray -you created a Gulp task.");
-})
-
-gulp1.task('html', function(){
-	console.log("Imagine something useful done to your HTML here.");
-})
-*/
+require ('./gulp/tasks/sprites');
